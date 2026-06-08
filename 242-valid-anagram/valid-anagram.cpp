1af3fef213cc61c33b16c0cba2e8c90084ab71cc
bhanumishra1234 <bhanu.mishradeep@gmail.com>
2026-06-08 14:02:57 +0530

    Time: 1 ms (54.06%) | Memory: 9.7 MB (75.27%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        int counts[26] = {0};
+        for(int i=0; i<s.size(); i++){
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--;
+        }
+        bool result = true;
+        for(int i=0; i<26; i++){
+            if(counts[i] != 0){
+                result = false;
+            }
+        }
+        return result;
+    }
+};
