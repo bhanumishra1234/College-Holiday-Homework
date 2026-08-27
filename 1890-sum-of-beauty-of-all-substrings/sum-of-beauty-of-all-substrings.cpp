52734ca69aa49355e1f3b61c63dc565e07770a13
bhanumishra1234 <bhanu.mishradeep@gmail.com>
2026-08-27 13:36:31 +0530

    Time: 127 ms (21.29%) | Memory: 26 MB (30.00%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    int beauty(string &s, int l, int r){
-        int freq[26] = {0};
-        for(int i=l; i<=r; i++){
-            freq[s[i] - 'a']++;
-        }
-        int maxi = 0;
-        int mini = INT_MAX;
-        for(int i=0; i<26; i++){
-            if(freq[i] > 0){
-                maxi = max(maxi, freq[i]);
-                mini = min(mini, freq[i]);
+    int beautySum(string s) {
+        int n = s.length();
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            unordered_map<char, int> freq;
+            for(int j=i; j<n; j++){
+                freq[s[j]]++;
+                int maxi = INT_MIN;
+                int mini = INT_MAX;
+                for(auto it:freq){
+                    mini = min(mini, it.second);
+                    maxi = max(maxi, it.second);
+                }
+                sum += maxi - mini;
             }
         }
-        return maxi - mini;
-    }
-    int func(string &s, int l, int r){
-        int n = s.size();
-        if(l >= n) return 0;
-        if(r >= n) return func(s, l + 1, l + 1);
-        return beauty(s, l, r) + func(s, l, r + 1);
-    }
-    int beautySum(string s) {
-        return func(s, 0, 0);
+        return sum;
     }
 };
